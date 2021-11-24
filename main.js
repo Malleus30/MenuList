9a9f8dd51642a71499cd1c9468620fcc9c852ab1
Anton <anton-lider@mail.ru>
2021-11-24 16:43:43 +0500

    'new'

---
+++ main.js
@@ -0,0 +1,11 @@
+/*let menuElem = document.getElementById('sweeties');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+  menuElem.classList.toggle('open');
+};*/
+
+
+title.addEventListener('click', function(){
+    sweeties.classList.toggle('open');
+})
